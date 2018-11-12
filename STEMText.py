db11d0343b5efd749fcc6eb6472dbb4129ef9d2d
Unknown <cwalk84@lsu.edu>
2018-11-12 17:39:10 -0600

    The Basics
    Text editor, incomplete but with the ability to edit text and some menus added (Most don't work because they have no menu options attached to them).

---
+++ STEMText.py
@@ -0,0 +1,64 @@
+import wx
+class MyPanel(wx.Panel):
+    def __init__(self):
+        self.SetBackgroundColour(wx.LIGHT_GREY)
+
+
+class MyFrame(wx.Frame):
+    def on_about(self, event):
+        dlg = wx.MessageDialog(self, "U fuckin wot, m8?", "About TechText", wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def on_exit(self, event):
+        self.Close(True)
+
+    def on_saveas(self, event):
+        print("Something")
+
+    def on_dark(self, event):
+        print("Something")
+
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(500, 500))
+        panel = wx.Panel
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        menubar = wx.MenuBar()
+
+        filemenu = wx.Menu()
+        aboutitem = filemenu.Append(wx.ID_ABOUT, "About", "Information about this program")
+        self.Bind(wx.EVT_MENU, self.on_about, aboutitem)
+        filemenu.AppendSeparator()
+        saveasitem = filemenu.Append(wx.ID_SAVEAS, "Save As", "Save this document as a new file")
+        self.Bind(wx.EVT_MENU, self.on_saveas, saveasitem)
+        filemenu.AppendSeparator()
+        exititem = filemenu.Append(wx.ID_EXIT, "Exit", "Terminate the program")
+        self.Bind(wx.EVT_MENU, self.on_exit, exititem)
+        filemenu.AppendSeparator()
+        menubar.Append(filemenu, "&File")
+
+        editmenu = wx.Menu()
+        menubar.Append(editmenu, "&Edit")
+
+        viewmenu = wx.Menu()
+        darkitem = viewmenu.Append(wx.ID_APPLY, "Dark Theme", "Set Dark Theme")
+        self.Bind(wx.EVT_MENU, self.on_dark, darkitem)
+        menubar.Append(viewmenu, "&View")
+
+        spec_char_menu = wx.Menu()
+        menubar.Append(spec_char_menu, "&Special Characters")
+
+        graphmenu = wx.Menu()
+        menubar.Append(graphmenu, "&Graphs")
+
+        codemenu = wx.Menu()
+        menubar.Append(codemenu, "&Code")
+
+        self.SetMenuBar(menubar)
+        self.Show(True)
+
+
+app = wx.App(False)
+frame = MyFrame(None, 'TechText')
+frame.Center()
+app.MainLoop()
