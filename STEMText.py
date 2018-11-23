4298ef2df3e80e97a87849f091b06d4a52c34c53
Unknown <cwalk84@lsu.edu>
2018-11-22 21:04:08 -0600

    Restructured MyFrame, created MyPanel
    Nothing really changed in the GUI, but the frame now has a sizer which is utilized when adding the graph toolboxes. Not pushing the toolboxes, so there is 1 error when you click on the "table toolbox" menu option under the graph menu.

--- STEMText.py
+++ STEMText.py
@@ -1,6 +1,32 @@
 import wx
 import tab
-import wx.lib.inspection
+#import TableToolbox
+
+class MyPanel(wx.Panel):
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        self.frame = parent
+        self.widgetSizer = wx.BoxSizer(wx.VERTICAL)
+        notebook = tab.NotebookPanel(self)
+        self.mainsizer = wx.BoxSizer(wx.VERTICAL)
+        self.mainsizer.Add(notebook, 1, wx.ALL | wx.EXPAND, 5)
+        self.mainsizer.Add(self.widgetSizer, 0, wx.CENTER | wx.ALL, 10)
+        self.SetSizer(self.mainsizer)
+
+"""
+    This method adds the toolbox that is used to add tables, but I'm not
+    pushing it yet because it doesn't really do anything atm.
+    
+    def add_table(self):
+        #new_button = wx.Button(self, label="notin", name="IDK")
+        toolbox = TableToolbox.TableToolbox(self)
+        self.widgetSizer.Add(toolbox, 0, wx.ALL, 5)
+        self.frame.fSizer.Layout()
+"""
+
+
+
 class MyFrame(wx.Frame):
     def on_about(self, event):
         dlg = wx.MessageDialog(self, "U fuckin wot, m8?", "About TechText", wx.OK)
@@ -16,14 +42,20 @@ class MyFrame(wx.Frame):
     def on_dark(self, event):
         print("Something")
 
+    def on_table(self, event):
+        self.panel.add_table()
+
+
+
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(500, 500))
+
+
         #The Tabs
-        panel = wx.Panel(self)
-        notebook = tab.NotebookPanel(panel)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(notebook, 1, wx.ALL | wx.EXPAND, 5)
-        panel.SetSizer(sizer)
+        self.panel = MyPanel(self)
+        self.fSizer = wx.BoxSizer(wx.VERTICAL)
+        self.fSizer.Add(self.panel, 1, wx.EXPAND)
+        self.SetSizer(self.fSizer)
 
         #The Menu
         menubar = wx.MenuBar()
@@ -51,6 +83,8 @@ class MyFrame(wx.Frame):
         menubar.Append(spec_char_menu, "&Special Characters")
 
         graphmenu = wx.Menu()
+        tableitem = graphmenu.Append(wx.ID_APPLY, "Table Toolbox", "Open the toolbox used for creating tables.")
+        self.Bind(wx.EVT_MENU, self.on_table, tableitem)
         menubar.Append(graphmenu, "&Graphs")
 
         codemenu = wx.Menu()
